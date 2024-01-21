97f72517b3e02b03e3037aed4cd67a7bce0faaab
JAtharva22 <me.atharvajadhav@gmail.com>
2024-01-21 21:22:26 +0530

    minor index.js

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,11 @@
 const cors = require('cors');
 const express = require('express');
-const http = require('http'); // Add http module
-const socketIO = require('socket.io');
 const connectToMongo = require('./db');
 
 require('dotenv').config();
 const PORT = process.env.PORT;
 
-const app = express();
-const server = http.createServer(app); // Create an HTTP server
-
-const io = socketIO(server); // Initialize Socket.io
+const app = express()
 
 app.use(cors());
 app.use(express.json());
@@ -19,13 +14,9 @@ connectToMongo();
 
 // Routes
 app.use('/api/auth', require('./routes/auth'))
-app.use('/', require('./routes/task'))
+app.use('/api/task', require('./routes/task'))
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  // Additional logic for handling specific events or actions from clients
-});
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
-});
+})
