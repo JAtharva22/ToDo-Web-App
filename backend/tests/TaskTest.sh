ae60dd0a96f385a72bb9e528f775865f1e59bda1
JAtharva22 <me.atharvajadhav@gmail.com>
2024-01-22 01:31:05 +0530

    Formatting

--- backend/tests/TaskTest.sh
+++ backend/tests/TaskTest.sh
@@ -1,7 +1,10 @@
+# CREATE : --------------------------------------------------
 # add task. auth token from signin/login required
 curl -X POST -H "Content-Type: application/json" -H "auth-token: " -d '{"title": "test1", "tags": ["work"], "description": "this is for testing", "startDate": "2024-01-20", "endDate": "2024-01-22"}' http://localhost:5000/addtask
 curl -X POST -H "Content-Type: application/json" -H "auth-token: " -d '{"title": "test2", "tags": ["school"], "description": "this is for testing", "startDate": "2024-01-20", "endDate": "2024-01-22"}' http://localhost:5000/addtask
 
+
+# READ : ----------------------------------------------------
 # get all task. auth token from signin/login required
 curl -X GET -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/gettask
 
@@ -19,10 +22,14 @@ curl -X GET -H "Content-Type: application/json" -H "auth-token: " http://localho
 curl -X GET -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/tasksearch/:key
 curl -X GET -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/tasksearch/work
 
+
+# UPDATE: -----------------------------------------------------
 # update task. auth token from signin/login required
 # replace taskId by appropriate ID in url(you can get id of tasks from above curl test) 
 curl -X PUT -H "Content-Type: application/json" -H "auth-token: " -d '{"title": "working"}' http://localhost:5000/updatetask/:taskId
 
+
+# DELETE: ------------------------------------------------------
 # delete task. auth token from signin/login required
 # replace taskId by appropriate ID in url(you can get id of tasks from above curl test) 
 curl -X DELETE -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/deletetask/:taskId
