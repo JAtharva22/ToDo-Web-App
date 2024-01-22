7aed6dd01f51ef549025b2cbdb3369549eb1966f
JAtharva22 <me.atharvajadhav@gmail.com>
2024-01-22 22:57:12 +0530

    only backend

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,10 @@
 # ToDo-Web-App
+
+<h1>Deployed URL</h1>
+<ul>
+<h4>
+<strong>
+https://todobackend-nwsy.onrender.com
+</strong>
+</h4>
+</ul>
--- backend/tests/TaskTest.sh
+++ backend/tests/TaskTest.sh
@@ -1,42 +1,42 @@
 # CREATE : --------------------------------------------------
 # add task. auth token from signin/login required
-curl -X POST -H "Content-Type: application/json" -H "auth-token: " -d '{"title": "test1", "tags": ["work"], "description": "this is for testing", "startDate": "2024-01-20", "endDate": "2024-01-22"}' http://localhost:5000/addtask
-curl -X POST -H "Content-Type: application/json" -H "auth-token: " -d '{"title": "test2", "tags": ["school"], "description": "this is for testing", "startDate": "2024-01-20", "endDate": "2024-01-22"}' http://localhost:5000/addtask
+curl -X POST -H "Content-Type: application/json" -H "auth-token: " -d '{"title": "test1", "tags": ["work"], "description": "this is for testing", "startDate": "2024-01-20", "endDate": "2024-01-22"}' https://todobackend-nwsy.onrender.com/addtask
+curl -X POST -H "Content-Type: application/json" -H "auth-token: " -d '{"title": "test2", "tags": ["school"], "description": "this is for testing", "startDate": "2024-01-20", "endDate": "2024-01-22"}' https://todobackend-nwsy.onrender.com/addtask
 
 
 # READ : ----------------------------------------------------
 # get all task. auth token from signin/login required
-curl -X GET -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/gettask
+curl -X GET -H "Content-Type: application/json" -H "auth-token: " https://todobackend-nwsy.onrender.com/gettask
 
 # get one task by id. auth token from signin/login required
 # replace taskId by appropriate ID in url(you can get id of tasks from above curl test) 
-curl -X GET -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/gettask/:taskId
+curl -X GET -H "Content-Type: application/json" -H "auth-token: " https://todobackend-nwsy.onrender.com/gettask/:taskId
 
 # get one task by tags. auth token from signin/login required
 # replace tag by appropriate tag in url
-curl -X GET -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/gettaskbytags/:tag
-curl -X GET -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/gettaskbytags/work
+curl -X GET -H "Content-Type: application/json" -H "auth-token: " https://todobackend-nwsy.onrender.com/gettaskbytags/:tag
+curl -X GET -H "Content-Type: application/json" -H "auth-token: " https://todobackend-nwsy.onrender.com/gettaskbytags/work
 
 # get tasks by search. auth token from signin/login required
 # replace value by appropriate tag or title in url
-curl -X GET -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/tasksearch/?key=value 
-curl -X GET -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/tasksearch/?key=work 
+curl -X GET -H "Content-Type: application/json" -H "auth-token: " https://todobackend-nwsy.onrender.com/tasksearch/?key=value 
+curl -X GET -H "Content-Type: application/json" -H "auth-token: " https://todobackend-nwsy.onrender.com/tasksearch/?key=work 
 
 
 # UPDATE: -----------------------------------------------------
 # update task. auth token from signin/login required
 # replace taskId by appropriate ID in url(you can get id of tasks from above curl test) 
-curl -X PUT -H "Content-Type: application/json" -H "auth-token: " -d '{"title": "working"}' http://localhost:5000/updatetask/:taskId
+curl -X PUT -H "Content-Type: application/json" -H "auth-token: " -d '{"title": "working"}' https://todobackend-nwsy.onrender.com/updatetask/:taskId
 
 
 # DELETE: ------------------------------------------------------
 # delete task. auth token from signin/login required
 # replace taskId by appropriate ID in url(you can get id of tasks from above curl test) 
-curl -X DELETE -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/deletetask/:taskId
+curl -X DELETE -H "Content-Type: application/json" -H "auth-token: " https://todobackend-nwsy.onrender.com/deletetask/:taskId
 
 # delete all task. auth token from signin/login required
-curl -X DELETE -H "Content-Type: application/json" -H "auth-token: " http://localhost:5000/deletealltask
+curl -X DELETE -H "Content-Type: application/json" -H "auth-token: " https://todobackend-nwsy.onrender.com/deletealltask
 
 # delete task based on tag. auth token from signin/login required
 # replace taskId by appropriate ID in url(you can get id of tasks from above curl test) 
-curl -X DELETE -H "Content-Type: application/json" -H "auth-token: " -d '{"tag": "work"}' http://localhost:5000/deletetagstask/
+curl -X DELETE -H "Content-Type: application/json" -H "auth-token: " -d '{"tag": "work"}' https://todobackend-nwsy.onrender.com/deletetagstask/
