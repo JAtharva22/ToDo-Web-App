98f535b464c439ed7003515f24cff6e62497dfcc
JAtharva22 <me.atharvajadhav@gmail.com>
2024-01-22 23:54:02 +0530

    minor

--- backend/tests/AuthTest.sh
+++ backend/tests/AuthTest.sh
@@ -1,8 +1,8 @@
 # sign in
-curl -X POST -H "Content-Type: application/json" -d '{"username": "test1", "password": "test1", "email": "test@gmail.com"}' https://todobackend-nwsy.onrender.com/api/auth/createuser
+curl -X POST -H "Content-Type: application/json" -d '{"username": "test1", "password": "test1", "email": "testing@gmail.com"}' https://todobackend-nwsy.onrender.com/api/auth/createuser
 
 # log in
-curl -X POST -H "Content-Type: application/json" -d '{"email": "test@gmail.com", "password": "test1"}' https://todobackend-nwsy.onrender.com/api/auth/login
+curl -X POST -H "Content-Type: application/json" -d '{"email": "testing@gmail.com", "password": "test1"}' https://todobackend-nwsy.onrender.com/api/auth/login
 
 # update username
 curl -X PUT -H "Content-Type: application/json" -H "auth-token: " -d '{"username": "abhi"}' https://todobackend-nwsy.onrender.com/api/auth/updateuser
